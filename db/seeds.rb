15b22cafe45c211b3ccecca4057dbcf92afb985e
ESASHIKA Kaoru <e_kaoru5@yahoo.co.jp>
2020-09-09 17:13:27 +0900

    Add initial data for movies tdb table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Movie.destroy_all
+
+movies = []
+movies << Movie.create!(
+    title: '花郎＜ファラン＞',
+    description: '今から1500年前の新羅時代。賤民の村で暮らすムミョン（パク・ソジュン）は、生き別れた家族を捜したいという親友のマンムン（イ・グァンス）と共に都に潜入する。ところが、二人は禁軍に追われて深手を負い、マンムンは命を落とす。ムミョンはマンムンの本名“ソヌ”を名乗り、彼の妹であるアロ（Ara）を守るために生きようと決意する。'
+)
+movies << Movie.create!(
+    title: 'ドクターズ～恋する気持ち',
+    description: '母の死をきっかけに問題児となり祖母に引き取られることになったヘジョンは、祖母宅に下宿していた風変りでお節介な元医師で教師のジホンと出会う。'
+)
